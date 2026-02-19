e97f5e6f04b62aec2cb1c2be56937b2b51ecf0ad
Robi <robijenik@gmail.com>
2026-02-19 14:27:15 -0500

    NEQ Op
    Added NEQ and tests/neq.morpho

--- src/main.cpp
+++ src/main.cpp
@@ -197,6 +197,13 @@ static dyn_var<int> morpho_vm(const int n, const uint32_t instructions[], object
 
                 reg[a] = X_MORPHO_BOOL(!x_morpho_extendedcomparevalue(left, right));
                 break;
+            case OP_NEQ:
+                a=DECODE_A(bc); b=DECODE_B(bc); c=DECODE_C(bc);
+                left = reg[b];
+                right = reg[c];
+
+                reg[a] = X_MORPHO_BOOL(x_morpho_extendedcomparevalue(left, right));
+                break;
             case OP_NOT:
                 a=DECODE_A(bc); b=DECODE_B(bc);
                 left = reg[b];
