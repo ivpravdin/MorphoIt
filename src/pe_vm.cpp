37d644333c20144e45723dce6dc22e1004813125
Robi <robijenik@gmail.com>
2026-04-10 21:50:15 -0400

    Higher order functions

--- src/pe_vm.cpp
+++ src/pe_vm.cpp
@@ -9,31 +9,43 @@
 // Include the BuildIt types
 using builder::dyn_var;
 using builder::static_var;
+using std::vector;
+
+typedef struct vm_return {
+    dyn_var<value> ret_dyn;
+    const value ret_stat;
+} vm_return;
+
+// I don't want this to be a method because that promotes the struct to a class
+// and there is some difference between raw struct "aggregate types" and classes
+// ...I think
+inline vm_return vm_return_with_val(const value v) {
+    return vm_return { .ret_dyn = (v) , .ret_stat = (v) };
+}
 
 constexpr size_t NUM_REGS = 255;
 constexpr size_t NUM_GLOBALS = 100;
 
 
-dyn_var<value> morpho_vm_rec(
+vm_return morpho_vm_rec(
     const int n,
     const instruction * const instructions,
     const objectfunction * const globalfn,
     dyn_var<value[NUM_GLOBALS]> &globals,
-    dyn_var<value[NUM_REGS]> &reg
+    static_var<value> globals_stat[],
+    dyn_var<value[NUM_REGS]> &reg,
+    static_var<value> reg_stat[]
 ) {
     // dyn_var<value> left = builder::with_name("left", true), right = builder::with_name("right", true);
     dyn_var<value> left, right;
 
+    // const int nregs = globalfn->nregs;
 
     // This doesn't seem to cause infinite loops
-    static_var<value> reg_stat[NUM_REGS];
-    for (static_var<size_t> i = 0; i < NUM_REGS; i++) {
-        reg_stat[i] = MORPHO_NIL;
-    }
-    static_var<value> globals_stat[NUM_GLOBALS];
-    for (static_var<size_t> i = 0; i < NUM_GLOBALS; i++) {
-        globals_stat[i] = MORPHO_NIL;
-    }
+    // static_var<value> globals_stat[NUM_GLOBALS];
+    // for (static_var<size_t> i = 0; i < NUM_GLOBALS; i++) {
+    //     globals_stat[i] = MORPHO_NIL;
+    // }
 
     static_var<instruction> pc = globalfn->entry;
 
@@ -56,58 +68,37 @@ dyn_var<value> morpho_vm_rec(
                     reg_stat[a] = globalfn->konst.data[bx];
                 }
                 break;
-            /* OPCODE: ADD
-             * 
-             * To-Do:
-             *      - Finish string concatenation?
-             *      - Object operator redirection
-             */
+
             case OP_ADD:
                 left = reg[b], right = reg[c];
                 reg[a] = runtime::op_add(left, right);
                 break;
-            /* OPCODE: SUBTRACT
-             * 
-             * To-Do:
-             *      - Object operator redirection
-             */
+
             case OP_SUB:
                 left = reg[b], right = reg[c];
 
                 reg[a] = runtime::op_sub(left, right);
                 break;
-            /* OPCODE: MULTIPLY
-             * 
-             * To-Do:
-             *      - Object operator redirection
-             */
+
             case OP_MUL:
                 left = reg[b], right = reg[c];
                 reg[a] = runtime::op_mul(left, right);
 
                 break;
-            /* OPCODE: DIVIDE
-             * 
-             * To-Do:
-             *      - Object operator redirection
-             */
+
             case OP_DIV:
                 left = reg[b], right = reg[c];
                 reg[a] = runtime::op_div(left, right);
 
                 break;
-            /* OPCODE: POW
-             * 
-             * To-Do:
-             *      - Verify implementation?
-             *      - Object operator redirection
-             */
+
             case OP_POW: //POW
                 left = reg[b];
                 right = reg[c];
                 reg[a] = runtime::op_pow(left, right);
 
                 break;
+
             case OP_EQ:
                 left = reg[b];
                 right = reg[c];
@@ -135,11 +126,7 @@ dyn_var<value> morpho_vm_rec(
             case OP_LE: //LT
                 left = reg[b];
                 right = reg[c];
-                // TODO: Type Errors
-                // if ( !( (MORPHO_ISFLOAT(left) || MORPHO_ISINTEGER(left)) &&
-                //        (MORPHO_ISFLOAT(right) || MORPHO_ISINTEGER(right)) ) ) {
-                //     OPERROR("Compare");
-                // }
+
                 reg[a] = X_MORPHO_BOOL(runtime::morpho_extendedcomparevalue(left, right) >= MORPHO_EQUAL);
                 break;
             case OP_B: // B
@@ -158,36 +145,55 @@ dyn_var<value> morpho_vm_rec(
 
             case OP_CALL: // CALL (no support for optional arguments yet)
                 left = reg[a];
-                runtime::print(reg[a]);
+                // runtime::print(reg[a]);          // print fn at runtime
+                // object_print(NULL, reg_stat[a]); // print fn at PE time
+    
                 // in theory should also handle closures
                 if (MORPHO_ISFUNCTION(reg_stat[a])) {
                     const objectfunction *func = MORPHO_GETFUNCTION(reg_stat[a]);
                     // TODO: this does not handle functions as returned values
                     dyn_var<value[NUM_REGS]> regswithargs;
+                    static_var<value> regswithargs_stat[NUM_REGS];
                     // r0 = function object
                     // I could pass it reg_stat but
                     // that would be the literal pointer to the function
                     regswithargs[0] = reg[a]; 
+                    regswithargs_stat[0] = reg_stat[a]; 
                     // r1..rn = args 1..n
-                    const int nargs = func->nargs;
-                    for (static_var<int> i = 0; i < nargs; i++) {
+                    const int32_t n_opt_args = b;
+                    const int32_t n_pos_args = c;
+                    for (static_var<size_t> i = 0; i < n_opt_args + n_pos_args; i++) {
                         regswithargs[i + 1] = reg[a + 1 + i];
+                        regswithargs_stat[i + 1] = reg_stat[a + 1 + i];
+                    }
+                    // Zero out rest of static regs
+                    for (static_var<size_t> i = n_opt_args + n_pos_args + 1; i < NUM_REGS; i++) {
+                        regswithargs_stat[i] = MORPHO_NIL;
                     }
 
-                    reg[a] = morpho_vm_rec(n, instructions, func, globals, regswithargs);
+                    vm_return retv = morpho_vm_rec(
+                        n,
+                        instructions,
+                        func,
+                        globals,
+                        globals_stat,
+                        regswithargs,
+                        regswithargs_stat
+                    );
+                    reg[a] = retv.ret_dyn;
+                    reg_stat[a] = retv.ret_stat;
                 } else {
                     reg[a]=runtime::call(left, b, reg + a);
                 }
                 break;
             case OP_RETURN:
-                if (a>0) {
-                    // so for proper fn behavior this should definitely return
-                    // a static value as well with a function, if there is one
-                    // but to implement this is highly highly annoying
-                    return reg[b];
-                } else {
-                    return MORPHO_NIL; /* No return value; returns nil */
-                }
+                // ret_stat SHOULD BE A VALID FUNCTION OR NIL
+                // ASSUMING reg_stat IS INITIALIZED TO BE ALL NIL
+                // and we do want it to be well-initialized for static-tagging
+                if (a>0)
+                    return vm_return { .ret_dyn = reg[b] , .ret_stat = reg_stat[b] };
+                else
+                    return vm_return_with_val(MORPHO_NIL);
                 break;
             case OP_LGL: // LGL
                 reg[a]=globals[bx];
@@ -206,20 +212,52 @@ dyn_var<value> morpho_vm_rec(
                 runtime::print(left);
                 break;
             case OP_END: // END
-                return EXIT_SUCCESS;
+                return vm_return_with_val(EXIT_SUCCESS);
             default:
                 runtime::printerr("Encountered unimplemented instruction. Exiting.");
-                return EXIT_FAILURE;
+                return vm_return_with_val(EXIT_FAILURE);
         }
 
         pc++;
     }
     runtime::printerr("Program counter exceeded bytecode buffer. Exiting.");
-    return EXIT_FAILURE;
+    return vm_return_with_val(EXIT_FAILURE);
 }
 
-dyn_var<value> morpho_vm(const int n, const instruction *const instructions, const objectfunction *const globalfn) {
-    dyn_var<value[NUM_GLOBALS]> globals; // = builder::with_name("globals", true);
+dyn_var<value> morpho_vm(
+    const int n,
+    const instruction *const instructions,
+    const objectfunction *const globalfn
+) {
+    dyn_var<value[NUM_GLOBALS]> globals = builder::with_name("globals", true);
     dyn_var<value[NUM_REGS]> reg;
-    return morpho_vm_rec(n, instructions, globalfn, globals, reg);
+
+    // const int nregs = globalfn->nregs;
+    // vector<static_var<value>> reg_stat(nregs);
+    // for (static_var<size_t> i = 0; i < nregs; i++) {
+    //     reg_stat[i] = MORPHO_NIL;
+    // }
+    static_var<value> globals_stat[NUM_GLOBALS];
+    for (static_var<size_t> i = 0; i < NUM_GLOBALS; i++) {
+        globals_stat[i] = MORPHO_NIL;
+    }
+
+    // this could be a vector of just the required number of regs?
+    // Idk how variadic arguments play with this. But certainly there is an nregs
+    // field in the function object
+    // I think that's street legal
+    static_var<value> reg_stat[NUM_REGS];
+    for (static_var<size_t> i = 0; i < NUM_REGS; i++) {
+        reg_stat[i] = MORPHO_NIL;
+    }
+
+    return morpho_vm_rec(
+        n,
+        instructions,
+        globalfn,
+        globals,
+        globals_stat,
+        reg,
+        reg_stat
+    ).ret_dyn;
 }
